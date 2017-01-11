2a0e0b398c82dd4f2f73eb21db0a4197ecfa2a05
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-11 17:39:49 +0100

    add player equal and move

--- app/src/main/java/com/xp/goose_game/Player.java
+++ app/src/main/java/com/xp/goose_game/Player.java
@@ -4,5 +4,46 @@ package com.xp.goose_game;
  * Created by XPeppers on 11/01/2017.
  */
 
-public class Player {
+class Player {
+
+    private final String name;
+
+    private int position;
+
+    public Player(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return name != null ? name.equals(player.name) : player.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void move(int firstDistance, int secondDistance) {
+        this.position+=firstDistance+secondDistance;
+    }
 }
--- app/src/test/java/com/xp/goose_game/PlayerTest.java
+++ app/src/test/java/com/xp/goose_game/PlayerTest.java
@@ -1,5 +1,7 @@
 package com.xp.goose_game;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +9,31 @@ import static org.junit.Assert.*;
  */
 public class PlayerTest {
 
+    @Test
+    public void can_create_a_player() throws Exception {
+        Player player = new Player("Pippo");
+        assertEquals("Pippo", player.getName());
+        assertEquals(0, player.getPosition());
+    }
+
+    @Test
+    public void players_with_same_name_is_the_same_player() throws Exception {
+        Player player = new Player("Pippo");
+        Player clone = new Player("Pippo");
+        assertEquals(true, player.equals(clone));
+    }
+
+    @Test
+    public void players_with_different_name_are_different_players() throws Exception {
+        Player player = new Player("Pippo");
+        Player player2 = new Player("Pluto");
+        assertEquals(false, player.equals(player2));
+    }
+
+    @Test
+    public void move_player_from_starting_position() throws Exception {
+        Player player = new Player("Pippo");
+        player.move(4,2);
+        assertEquals(6, player.getPosition());
+    }
 }
