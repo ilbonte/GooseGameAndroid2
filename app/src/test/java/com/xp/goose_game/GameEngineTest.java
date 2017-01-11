9652d2ab8f128ad3d0f051b65610c16d46e34c41
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-11 16:44:16 +0100

    refactor GameEngineTest

--- app/src/test/java/com/xp/goose_game/GameEngineTest.java
+++ app/src/test/java/com/xp/goose_game/GameEngineTest.java
@@ -1,5 +1,6 @@
 package com.xp.goose_game;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -7,45 +8,47 @@ import static junit.framework.Assert.assertEquals;
 
 public class GameEngineTest {
 
+    GameEngine engine;
+
+    @Before
+    public void setUp() throws Exception {
+        engine= new GameEngine();
+    }
+
     @Test
     public void add_player_to_game() throws Exception {
-        GameEngine engine = new GameEngine();
-
-        engine.action("aggiungi giocatore Pippo");
+        addPlayer("Pippo");
 
         assertEquals("Giocatori: Pippo", engine.state());
     }
 
     @Test
     public void add_two_players_to_game() throws Exception {
-        GameEngine engine = new GameEngine();
-
-        engine.action("aggiungi giocatore Pippo");
-        engine.action("aggiungi giocatore Pluto");
+        addPlayer("Pippo");
+        addPlayer("Pluto");
 
         assertEquals("Giocatori: Pippo, Pluto", engine.state());
     }
-
-
+    
     @Test
     public void adds_repeating_player() throws Exception {
-        GameEngine engine = new GameEngine();
-
-        engine.action("aggiungi giocatore Pippo");
-        engine.action("aggiungi giocatore Pippo");
+        addPlayer("Pippo");
+        addPlayer("Pippo");
 
         assertEquals("Pippo: giocatore già presente", engine.state());
     }
 
     @Test
     public void move_a_player_from_starting_position() throws Exception {
-        GameEngine engine = new GameEngine();
-        engine.action("aggiungi giocatore Pippo");
+        addPlayer("Pippo");
 
         engine.action("muovi Pippo 4, 2");
 
         assertEquals("Pippo tira 4, 2. Pippo muove da Partenza a 6", engine.state());
     }
 
+    private void addPlayer(String playerName){
+        engine.action("aggiungi giocatore "+playerName);
+    }
 
 }
