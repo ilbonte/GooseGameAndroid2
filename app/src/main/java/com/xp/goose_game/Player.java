1a88e2645c1a40e1794ac88a7875e1d5146b3347
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-11 16:50:42 +0100

    add player

---
+++ app/src/main/java/com/xp/goose_game/Player.java
@@ -0,0 +1,8 @@
+package com.xp.goose_game;
+
+/**
+ * Created by XPeppers on 11/01/2017.
+ */
+
+public class Player {
+}
---
+++ app/src/test/java/com/xp/goose_game/PlayerTest.java
@@ -0,0 +1,10 @@
+package com.xp.goose_game;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by XPeppers on 11/01/2017.
+ */
+public class PlayerTest {
+
+}
