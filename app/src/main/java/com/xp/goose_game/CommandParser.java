13fa47fddd2f24d232850715515402e4f42c5bfb
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-12 12:25:17 +0100

    refactor parser

--- app/src/main/java/com/xp/goose_game/CommandParser.java
+++ app/src/main/java/com/xp/goose_game/CommandParser.java
@@ -1,26 +1,67 @@
 package com.xp.goose_game;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by XPeppers on 11/01/2017.
  */
 
 public class CommandParser {
 
-    public static final String ADD_PLAYER_COMMAND = "aggiungi giocatore \\w+";
+    private static final String ADD_PLAYER_COMMAND = "aggiungi giocatore \\w+";
 
-    public static final String MOVE_PLAYER_COMMAND = "muovi \\w+ \\d, \\d";
+    private static final String MOVE_PLAYER_COMMAND = "muovi (\\w+) (\\d), (\\d)";
+    private final Command command;
+
+    private String parsedName;
+    private int parsedDiceValues [] = null;
+
+    public CommandParser(String command){
+        this.command = this.parse(command);
+    }
 
     public Command parse(String input) {
+
         if(input.matches(ADD_PLAYER_COMMAND)) {
+            this.parsedName = input.replace("aggiungi giocatore ", "");
+
             return Command.addPlayer;
         }
 
         if(input.matches(MOVE_PLAYER_COMMAND)) {
+            this.parsedName = parseNameFromMoveCommand(input);
+            this.parsedDiceValues = parseDiceFromMoveCommand(input);
             return Command.movePlayer;
         }
 
-
-
         return Command.invalid;
+    }
+
+    private int[] parseDiceFromMoveCommand(String input) {
+        Pattern p = Pattern.compile(MOVE_PLAYER_COMMAND);
+        Matcher m = p.matcher(input);
+        m.find();
+        return new int[]{Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3))};
+    }
+
+    private String parseNameFromMoveCommand(String input) {
+        Pattern p = Pattern.compile(MOVE_PLAYER_COMMAND);
+        Matcher m = p.matcher(input);
+        m.find();
+        return m.group(1);
+    }
+
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public String parseName() {
+        return this.parsedName;
+    }
+
+    public int[] parseDice() {
+        return this.parsedDiceValues;
     }
 }
--- app/src/test/java/com/xp/goose_game/CommandParserTest.java
+++ app/src/test/java/com/xp/goose_game/CommandParserTest.java
@@ -10,17 +10,52 @@ import static org.junit.Assert.*;
 public class CommandParserTest {
     @Test
     public void add_player_input_returns_add_player_command() throws Exception {
-        CommandParser commandParser = new CommandParser();
         String userInput = "aggiungi giocatore Pippo";
+        CommandParser commandParser = new CommandParser(userInput);
 
-        assertEquals(Command.addPlayer,commandParser.parse(userInput));
+        assertEquals(Command.addPlayer,commandParser.getCommand());
     }
 
     @Test
     public void move_player_input_returns_move_player_command() throws Exception {
-        CommandParser commandParser = new CommandParser();
         String userInput = "muovi Pippo 4, 2";
+        CommandParser commandParser = new CommandParser(userInput);
 
-        assertEquals(Command.movePlayer,commandParser.parse(userInput));
+        assertEquals(Command.movePlayer,commandParser.getCommand());
     }
+
+    @Test
+    public void invalid_command_is_detected() throws Exception {
+        String userInput = "invalid command";
+        CommandParser commandParser = new CommandParser(userInput);
+
+        assertEquals(Command.invalid,commandParser.getCommand());
+    }
+
+    @Test
+    public void can_extract_name_from_add_player_command() throws Exception {
+        String userInput = "aggiungi giocatore Pippo";
+        CommandParser commandParser = new CommandParser(userInput);
+
+        assertEquals("Pippo",commandParser.parseName());
+    }
+
+    @Test
+    public void can_extract_name_from_move_player_command() throws Exception {
+        String userInput = "muovi Pippo 4, 2";
+        CommandParser commandParser = new CommandParser(userInput);
+
+        assertEquals("Pippo",commandParser.parseName());
+    }
+
+    @Test
+    public void can_extract_dice_from_move_command() throws Exception {
+        String userInput = "muovi Pippo 4, 2";
+        CommandParser commandParser = new CommandParser(userInput);
+
+        assertEquals(4,commandParser.parseDice()[0]);
+        assertEquals(2,commandParser.parseDice()[1]);
+
+    }
+
 }
